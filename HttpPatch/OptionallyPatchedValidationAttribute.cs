a1924114158e74e23b2a9f3d1e051fbb1ad1ba37
Andrew Butler <butlerat@gmail.com>
2022-11-09 14:50:36 +0000

    Add additional validation attributes (#7)
    * EmailAddressIfPatched
      * StringLengthIfPatched
      * RegularExpressionIfPatched
    
    Co-authored-by: Andrew Butler <andrew.butler@ensono.com>

--- HttpPatch/OptionallyPatchedValidationAttribute.cs
+++ HttpPatch/OptionallyPatchedValidationAttribute.cs
@@ -56,3 +56,33 @@ public sealed class RangeIfPatchedAttribute : OptionallyPatchedValidationAttribu
         Maximum = maximum;
     }
 }
+
+public sealed class EmailAddressIfPatchedAttribute : OptionallyPatchedValidationAttribute
+{
+    public EmailAddressIfPatchedAttribute()
+        : base(new EmailAddressAttribute())
+    {
+    }
+}
+
+public sealed class StringLengthIfPatchedAttribute : OptionallyPatchedValidationAttribute
+{
+    public int MaximumLength { get; }
+
+    public StringLengthIfPatchedAttribute(int maximumLength) 
+        : base(new StringLengthAttribute(maximumLength))
+    {
+        MaximumLength = maximumLength;
+    }
+}
+
+public sealed class RegularExpressionIfPatchedAttribute : OptionallyPatchedValidationAttribute
+{
+    public string Pattern { get; }
+
+    public RegularExpressionIfPatchedAttribute(string pattern) 
+        : base(new RegularExpressionAttribute(pattern))
+    {
+        Pattern = pattern;
+    }
+}
